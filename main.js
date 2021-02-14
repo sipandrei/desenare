f0ba2871da4db409b1c8347b0182023a63df7f0f
sipandrei <pontos.serban@gmail.com>
2021-02-14 20:42:42 +0200

    creare versiune 1 genereare

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
+const main = document.querySelector("main");
 function crearePixeli(linii, coloane) {
   for (let i = 0; i < linii; i++) {
-    const linie = document.createElement("div");
+    const linie = document.createElement("span");
+    linie.classList.add("linie");
     for (let j = 0; j < coloane; j++) {
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
 
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = alegereCuloare();
+      });
       linie.appendChild(pixel);
     }
-    document.body.appendChild(linie);
+    main.appendChild(linie);
   }
 }
-crearePixeli(16, 16);
+function alegereCuloare() {
+  let color =
+    "rgb(" +
+    Math.round(Math.random() * 255) +
+    "," +
+    Math.round(Math.random() * 255) +
+    "," +
+    Math.round(Math.random() * 255) +
+    ")";
+
+  return color;
+}
+crearePixeli(23, 23);
