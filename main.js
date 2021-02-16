1780bd02a2fca7447c4a469825881424994c8122
sipandrei <pontos.serban@gmail.com>
2021-02-16 12:17:02 +0200

    layout elemente si argumente optionale la creare pixeli

--- main.js
+++ main.js
@@ -11,18 +11,30 @@ function alegereCuloare() {
 
   return color;
 }
-function crearePixeli(linii, coloane) {
-  main.style.gridTemplateRows = `repeat(${linii}, 1fr)`;
-  main.style.gridTemplateColumns = `repeat(${coloane}, 1fr)`;
-  for (let j = 0; j < coloane * linii; j++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixel.style.width = 1 + "fr";
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = alegereCuloare();
-    });
-    main.appendChild(pixel);
+function crearePixeli(...arg) {
+  if (arg.length != 0) {
+    let linii = arg[0];
+    let coloane = linii; // la inceput facem patrat
+    if (arg.length > 1) {
+      // daca avem si numarul coloanelor, il folosim
+      coloane = arg[1];
+    }
+    main.style.gridTemplateRows = `repeat(${linii}, 1fr)`;
+    main.style.gridTemplateColumns = `repeat(${coloane}, 1fr)`;
+    for (let j = 0; j < coloane * linii; j++) {
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.style.width = 1 + "fr";
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = alegereCuloare();
+      });
+      main.appendChild(pixel);
+    }
+  } else {
+    while (main.hasChildNodes()) {
+      main.removeChild(main.firstChild);
+    }
   }
 }
 
-crearePixeli(64, 64);
+crearePixeli(10);
